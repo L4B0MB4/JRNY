fdb138d21339ed8e4218b36ee01a9accbcd148c3
L4B0MB4 <metalfan65@gmail.com>
2024-05-07 20:45:13 +0200

    jrny: setup gin

--- jrny/main.go
+++ jrny/main.go
@@ -1,9 +1,38 @@
 package main
 
 import (
-	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/rs/zerolog/log"
 )
 
+type Model struct {
+	Name string `json:"name" binding:"required"`
+}
+type InternalServerError struct {
+	Error string `json:"error"`
+}
+
+func onRequest(c *gin.Context) {
+	var model Model
+	err := c.ShouldBindBodyWithJSON(&model)
+	if err != nil {
+		log.Err(err)
+		iSE := InternalServerError{
+			Error: err.Error(),
+		}
+		c.JSON(500, &iSE)
+		return
+
+	}
+	c.Status(201)
+
+	log.Debug().Str("method", c.Request.Method).Str("path", c.Request.RequestURI).Interface("body", model).Msg("On Request")
+}
+
 func main() {
-	fmt.Println("hello")
+
+	router := gin.Default()
+	router.POST("/api/event", onRequest)
+	router.Run(":8080")
+
 }
