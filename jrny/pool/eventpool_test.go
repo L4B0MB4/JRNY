954ea2ddd64142b73178c147ca0355a8473b2fec
L4B0MB4 <metalfan65@gmail.com>
2024-05-09 16:16:40 +0200

    jrny: adding eventpool tests

---
+++ jrny/pool/eventpool_test.go
@@ -0,0 +1,29 @@
+package pool_test
+
+import (
+	"testing"
+
+	"github.com/L4B0MB4/JRNY/jrny/models"
+	"github.com/L4B0MB4/JRNY/jrny/pool"
+)
+
+func TestUnInitializedQueue(t *testing.T) {
+	ep := pool.EventPool{}
+	err := ep.Enqueue(&models.Event{})
+
+	if err == nil {
+		t.Error("no error for unitialized eventpool")
+	}
+
+}
+
+func TestInitializedQueue(t *testing.T) {
+	ep := pool.EventPool{}
+	ep.Initialize()
+	err := ep.Enqueue(&models.Event{})
+
+	if err != nil {
+		t.Error("error for initialzed eventpool")
+	}
+
+}
