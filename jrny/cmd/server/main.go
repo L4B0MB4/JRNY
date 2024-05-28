d3bb02a08a7be32d94d84d9a6b837a904e383a90
L4B0MB4 <metalfan65@gmail.com>
2024-05-28 16:10:37 +0200

    jryn: missing log setup

--- jrny/cmd/server/main.go
+++ jrny/cmd/server/main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"os"
+
 	"github.com/L4B0MB4/JRNY/jrny/pkg/configuration"
 	"github.com/L4B0MB4/JRNY/jrny/pkg/server"
 	"github.com/L4B0MB4/JRNY/jrny/pkg/server/pool/factory"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 )
 
 func main() {
@@ -12,6 +16,7 @@ func main() {
 }
 func setup() (*configuration.ServerConfiguration, factory.EventPoolWorkerFactory) {
 
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
 	config := configuration.DefaultServerConfiguration()
 	workerFactory := factory.RabbitMqEventPoolWorkerFactory{
 		Config: &config,
