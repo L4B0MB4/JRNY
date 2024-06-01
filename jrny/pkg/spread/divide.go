145b2529441aa98186909d29b863ea5380597fe4
L4B0MB4 <metalfan65@gmail.com>
2024-06-01 11:47:14 +0200

    jrny: dividing up guids in nearly equal spaces

--- jrny/pkg/spread/divide.go
+++ jrny/pkg/spread/divide.go
@@ -1,12 +1,12 @@
 package spread
 
-import "fmt"
-
 var min = 0x00000000
 var max = 0xFFFFFFFF
 
-func EqualDivideSpace(workerAmount int) {
-	extra := max % workerAmount
-	spaceSize := (max - extra) / workerAmount
-	fmt.Printf("%x / %x = %x and would have %v extra \n", max, workerAmount, spaceSize, extra)
+func EqualDivideSpace(workerAmount int) (firstSpace int, remainingSpaces int) {
+	maxSpaceSize := max + 1 //+1 due to including min into space size
+	leftOver := maxSpaceSize % workerAmount
+	spaceSize := (maxSpaceSize - leftOver) / workerAmount
+
+	return spaceSize + leftOver, spaceSize
 }
--- jrny/pkg/spread/divide_test.go
+++ jrny/pkg/spread/divide_test.go
@@ -3,10 +3,24 @@ package spread
 import "testing"
 
 func TestEqualDivideSpace(t *testing.T) {
-	EqualDivideSpace(1)
-	EqualDivideSpace(2)
-	EqualDivideSpace(3)
-	EqualDivideSpace(4)
-	EqualDivideSpace(5)
-	EqualDivideSpace(6)
+	firstSpace, remainingSpace := EqualDivideSpace(1)
+	if firstSpace != 0x100000000 || remainingSpace != firstSpace {
+		t.Error("Dividing up spaces does not create proper space sizes")
+	}
+	firstSpace, remainingSpace = EqualDivideSpace(2)
+	if firstSpace != 0x80000000 || remainingSpace != firstSpace {
+		t.Error("Dividing up spaces does not create proper space sizes")
+	}
+	firstSpace, remainingSpace = EqualDivideSpace(3)
+	if firstSpace != 0x55555556 || remainingSpace != (firstSpace-1) {
+		t.Error("Dividing up spaces does not create proper space sizes")
+	}
+	firstSpace, remainingSpace = EqualDivideSpace(4)
+	if firstSpace != 0x40000000 || remainingSpace != firstSpace {
+		t.Error("Dividing up spaces does not create proper space sizes")
+	}
+	firstSpace, remainingSpace = EqualDivideSpace(5)
+	if firstSpace != 0x33333334 || remainingSpace != (firstSpace-1) {
+		t.Error("Dividing up spaces does not create proper space sizes")
+	}
 }
