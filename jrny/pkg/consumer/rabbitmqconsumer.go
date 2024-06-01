bb0ee1ecf84cf098070463300b7e2a5bec5d4d83
L4B0MB4 <metalfan65@gmail.com>
2024-06-01 14:49:27 +0200

    jrny: add helper for queues

--- jrny/pkg/consumer/rabbitmqconsumer.go
+++ jrny/pkg/consumer/rabbitmqconsumer.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 
 	"github.com/L4B0MB4/JRNY/jrny/pkg/configuration"
+	"github.com/L4B0MB4/JRNY/jrny/pkg/helper"
 	"github.com/L4B0MB4/JRNY/jrny/pkg/models"
 	amqp "github.com/rabbitmq/amqp091-go"
 	"github.com/rs/zerolog/log"
@@ -36,13 +37,8 @@ func (c *RabbitMqConsumer) Initialize() error {
 		log.Error().Err(err).Msg("Could not connect open rabbitmq channel")
 		return err
 	}
-	_, err = c.channel.QueueDeclare("events", true, false, false, false, amqp.Table{amqp.QueueTypeArg: amqp.QueueTypeStream,
-		amqp.StreamMaxLenBytesArg:         int64(5_000_000_000), // 5 Gb
-		amqp.StreamMaxSegmentSizeBytesArg: 500_000_000,          // 500 Mb
-		amqp.StreamMaxAgeArg:              "1h",                 // 3 days
-	})
+	err = helper.CreateDefaultQueue(c.channel)
 	if err != nil {
-		log.Error().Err(err).Msg("Could not declare rabbitmq queue")
 		return err
 	}
 	c.lifeTimeContext = context.Background() //todo: change to context that is canceled on ctrl-c
