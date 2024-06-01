40da98de2ba3d61b4f3678aeb2a16ed86293adb2
L4B0MB4 <metalfan65@gmail.com>
2024-06-01 17:37:19 +0200

    jrny: adding guard for area of responsibility

--- jrny/pkg/merging/automatedmerging.go
+++ jrny/pkg/merging/automatedmerging.go
@@ -1,6 +1,8 @@
 package merging
 
 import (
+	"math/big"
+
 	"github.com/L4B0MB4/JRNY/jrny/pkg/models"
 	"github.com/L4B0MB4/JRNY/jrny/pkg/space"
 	"github.com/rs/zerolog/log"
@@ -18,11 +20,23 @@ func (s *SelfConfiguringMerging) SelfConfigure() {
 
 func (s *SelfConfiguringMerging) Initialize(r *space.ResponsibleArea) {
 	s.responsibleArea = r
-	s.knownIdentifiers = make(map[[16]byte]*models.IdentifierReference, ((r.To-r.From)/10000)+1)
+	s.knownIdentifiers = make(map[[16]byte]*models.IdentifierReference, 1000)
+}
+
+func (s *SelfConfiguringMerging) ResponsibleAreaGuard(id [16]byte) bool {
+	idBigInt := big.NewInt(0).SetBytes(id[:])
+	if idBigInt.Cmp(&s.responsibleArea.From) >= 0 && idBigInt.Cmp(&s.responsibleArea.To) == -1 {
+		return true
+	}
+	return false
 }
 
 func (s *SelfConfiguringMerging) Merge(event *models.Event) {
 	id := event.ID
+	if !s.ResponsibleAreaGuard(id) {
+		return
+	}
+
 	v, ok := s.knownIdentifiers[id]
 	if !ok {
 		v = &models.IdentifierReference{
--- jrny/pkg/merging/automatedmerging_test.go
+++ jrny/pkg/merging/automatedmerging_test.go
@@ -1,6 +1,7 @@
 package merging
 
 import (
+	"math/big"
 	"testing"
 
 	"github.com/L4B0MB4/JRNY/jrny/pkg/models"
@@ -15,21 +16,41 @@ func TestInitialize(t *testing.T) {
 		t.Errorf("Expected knownIdentifierArray to be uninitialized")
 	}
 	m.Initialize(&space.ResponsibleArea{
-		From: 1,
-		To:   4,
+		From: *big.NewInt(1),
+		To:   *big.NewInt(4),
 	})
 	if m.knownIdentifiers == nil {
 		t.Errorf("Expected knownIdentifierArray to be initialized")
 	}
 
 }
+func TestResponsibleAreaGuard(t *testing.T) {
+	m := SelfConfiguringMerging{}
+	m.Initialize(&space.ResponsibleArea{
+		From: *big.NewInt(1),
+		To:   *big.NewInt(4),
+	})
+	myuuid := uuid.MustParse("ed4c5e8f-c512-48ba-b488-bb4be07508e3")
+	_, ok := m.knownIdentifiers[myuuid]
+	if ok {
+		t.Error("Expected item not to be in knownIdentifiers")
+	}
+	m.Merge(&models.Event{
+		Type: "a-type",
+		ID:   myuuid,
+	})
+	_, ok = m.knownIdentifiers[myuuid]
+	if ok {
+		t.Error("Expected item not to be in knownIdentifiers due to it being out of range")
+	}
+}
 
 func TestMergingUnkowns(t *testing.T) {
 
 	m := SelfConfiguringMerging{}
 	m.Initialize(&space.ResponsibleArea{
-		From: 1,
-		To:   4,
+		From: *big.NewInt(1),
+		To:   *big.NewInt(0).SetBytes(uuid.Max[:]),
 	})
 	myuuid := uuid.MustParse("ed4c5e8f-c512-48ba-b488-bb4be07508e3")
 	_, ok := m.knownIdentifiers[myuuid]
