f3ed41cf5fd38138b8a23c35f355e3b4555356f4
L4B0MB4 <metalfan65@gmail.com>
2024-06-13 20:07:04 +0200

    deplyoment: ongoing development

--- pkg/configuration/rabbitmq.go
+++ pkg/configuration/rabbitmq.go
@@ -9,7 +9,14 @@ type RabbitMqConfiguration struct {
 func (c *RabbitMqConfiguration) Default() {
 	url, ok := os.LookupEnv("RABBITMQ_URL")
 	if !ok {
-		c.Endpoint = "amqp://guest:guest@localhost:5672/"
+		url, ok = os.LookupEnv("RABBIT_MQ__MQTT_SERVICE_SERVICE_HOST")
+		port, okPort := os.LookupEnv("RABBIT_MQ__MQTT_SERVICE_SERVICE_PORT")
+
+		if !ok || !okPort {
+			c.Endpoint = "amqp://guest:guest@localhost:5672/"
+		} else {
+			c.Endpoint = "amqp://guest:guest@" + url + ":" + string(port) + "/"
+		}
 
 	} else {
 		c.Endpoint = url
